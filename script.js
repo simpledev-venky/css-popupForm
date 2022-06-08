24d18aff4643e88c26638066d7e455fc0081d0ff
Your Name <you@example.com>
2022-06-08 19:43:24 +0530

    animated bg added,custom scrollbar addded

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSignForm() {
+    document.getElementById("mySignForm").style.display = "block";
+    document.querySelector(".signup-container").style.display = "block";
+  }
+
+  function closeSignForm() {
+    document.getElementById("mySignForm").style.display = "none";
+    document.querySelector(".signup-container").style.display = "none";
+  }
